935107c4b75db3ecaca84144964a524971c26b7b
perryabdulkadir <72145578+perryabdulkadir@users.noreply.github.com>
2021-03-21 19:11:37 -0500

    Add files via upload

---
+++ MechaCarChallenge.R
@@ -0,0 +1,5 @@
+library(dplyr)
+
+MechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+
+summary(lm(mpg~vehicle_length + vehicle_weight + spoiler_angle + ground_clearance  + AWD, MechaCar_mpg)) #summarize linear model
