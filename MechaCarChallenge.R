76d1004fceceee3b3bb472d640fb5ad5771c15c0
perryabdulkadir <72145578+perryabdulkadir@users.noreply.github.com>
2021-03-21 20:20:37 -0500

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,3 +9,14 @@ Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFa
 total_summary <- Suspension_Coil %>% summarize (Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))#create summary table
 
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize (Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))#create lot summary table
+
+t.test((Suspension_Coil$PSI), mu=1500) #compare sample versus population means
+
+lot1 <- Suspension_Coil[Suspension_Coil$Manufacturing_Lot == 'Lot1',]
+t.test((lot1$PSI), mu=1500) #compare sample versus population means
+
+lot2 <- Suspension_Coil[Suspension_Coil$Manufacturing_Lot == 'Lot2',]
+t.test((lot2$PSI), mu=1500) #compare sample versus population means
+
+lot3 <- Suspension_Coil[Suspension_Coil$Manufacturing_Lot == 'Lot3',]
+t.test((lot3$PSI), mu=1500) #compare sample versus population means
